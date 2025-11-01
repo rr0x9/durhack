04b5b7b7dd10a73b7088c8cf726ea75d8c230ba6
yesman2000s <mjayaed@gmail.com>
2025-11-01 18:06:42 +0000

    Colour Change

--- front-end/src/components/ChatMessage.jsx
+++ front-end/src/components/ChatMessage.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 export default function ChatMessage({ sender, text, sentiment }) {
   const isUser = sender === "user";
+
+  const colour = "#c43b1fff";
+
   return (
     <div className={`message-row ${isUser ? "user-message" : "bot-message"}`}>
-      <div className="message-bubble">{text}</div>
+      <div
+        className="message-bubble"
+        style={!isUser ? { backgroundColor: colour } : {}}
+      >
+        {text}
+      </div>
     </div>
   );
 }
