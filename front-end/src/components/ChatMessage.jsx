42f8dd44b97ad1eea15a1cdb0e7e319c9b866d2c
zak <zakebramley@gmail.com>
2025-11-01 18:24:34 +0000

    a

--- front-end/src/components/ChatMessage.jsx
+++ front-end/src/components/ChatMessage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function ChatMessage({ sender, text, sentiment }) {
-  let red = 0x440000;
-  let green = 0x004400;
-  let blue = 0x000044;
+  let red = 0x44;
+  let green = 0x44;
+  let blue = 0x44;
   if ( sentiment < 0 ) {
     red = Math.floor(-187*sentiment)+0x44;
   } else {
